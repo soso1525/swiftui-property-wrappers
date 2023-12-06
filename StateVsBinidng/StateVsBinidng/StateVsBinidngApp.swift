b63e0594ec42e7bcc8aa44542d677b3b704b9212
sohyun <sandy1525@naver.com>
2023-12-06 20:38:28 +0900

    @State vs @Binding

---
+++ StateVsBinidng/StateVsBinidng/StateVsBinidngApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateVsBinidngApp.swift
+//  StateVsBinidng
+//
+//  Created by 소현 on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct StateVsBinidngApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
